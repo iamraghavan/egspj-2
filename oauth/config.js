11aeebc019d488270ae5f0bdf332ab997d64adb9
iamraghavan <developer.raghavan@gmail.com>
2023-06-05 10:02:23 +0530

    10:00:AM 05:06:23 I

---
+++ oauth/config.js
@@ -0,0 +1,14 @@
+var config = {
+  apiKey: "AIzaSyC7EsiaYH-tJj-wNEQl8ZrrmdvKCWS7ztY",
+  authDomain: "egspj-bumblebees.firebaseapp.com",
+  projectId: "egspj-bumblebees",
+  storageBucket: "egspj-bumblebees.appspot.com",
+  messagingSenderId: "3531307553",
+  appId: "1:3531307553:web:162f525f3ed82d7830c160",
+  measurementId: "G-6W931XY0SQ"
+  };
+  firebase.initializeApp(config);
+
+// admin.initializeApp({
+//     credential: admin.credential.applicationDefault(),
+//   });
